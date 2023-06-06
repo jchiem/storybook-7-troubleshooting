74b0163c17721eb9c59028f7fb39e06fc708a2de
Joakim Chiem <joakim.chiem2@ingka.ikea.com>
2023-06-06 15:55:57 +0200

    chore: add example with dev.story.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,8 @@ const config = {
   stories: [
     "../stories/**/*.mdx", 
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../stories/**/*.story.@(js|jsx|ts|tsx)"
+    "../stories/**/*.story.@(js|jsx|ts|tsx)",
+    "../stories/**/*.dev.story.@(js|jsx|ts|tsx)"
   ],
   addons: [
     "@storybook/addon-links",
